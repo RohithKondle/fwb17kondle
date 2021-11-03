efa4a828407f539816416fedd9d195c915316ff7
RohithKondle <67865663+rohith1998-123@users.noreply.github.com>
2021-11-02 21:37:21 -0500

    updated flights page link and flights page

--- routes/flight.js
+++ routes/flight.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('Flight', { title: 'Flight Search Results' });
+  res.render('flight', { title: 'Flight Search Results' });
 });
 
 module.exports = router;
